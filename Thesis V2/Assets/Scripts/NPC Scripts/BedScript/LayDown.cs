d83f25bb13fef1aec05a25299b9384519c44ff53
Discord615 <vanbeau1@gmail.com>
2023-10-20 19:54:05 +0800

    Added Require Component on Bed and Chair scripts

--- Thesis V2/Assets/Scripts/NPC Scripts/BedScript/LayDown.cs
+++ Thesis V2/Assets/Scripts/NPC Scripts/BedScript/LayDown.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class LayDown : MonoBehaviour // TODO: LayDown Script - CLEAN UP CODE!!!!
 {
     public bool occupied = false;
--- Thesis V2/Assets/Scripts/NPC Scripts/Chair Script/SitDown.cs
+++ Thesis V2/Assets/Scripts/NPC Scripts/Chair Script/SitDown.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class SitDown : MonoBehaviour
 {
     private GameObject occupant = null;
