5de1c3ca42f3754e800e41cd43a44cc502b1101e
Discord615 <vanbeau1@gmail.com>
2023-10-22 14:36:33 +0800

    Added Inventory Logic
    Healing, Losing Health, And Restocking

---
+++ Thesis V2/Assets/Scripts/Managers/Hud Managers/InventoryManager.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class InventoryManager : MonoBehaviour, IDataPersistence
+{
+    public InventoryManager instance { get; private set; }
+
+    [SerializeField] private TextMeshProUGUI gloveText;
+    [SerializeField] private TextMeshProUGUI alcoholText;
+    [SerializeField] private TextMeshProUGUI maskText;
+    private int gloveRemainingUses;
+    private int alcoholRemainingUses;
+    private int maskRemainingUses;
+
+    public bool glovesInUse = false;
+    public bool maskInUse = false;
+
+    public bool itemsAreAvailable = false;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one instance of Inventory Manager exists in the current scene");
+        }
+    }
+
+    private void Update()
+    {
+        updateAllUsesText();
+
+        if (gloveRemainingUses <= 0 && glovesInUse)
+        {
+            glovesInUse = false;
+            StartCoroutine(cannotUse(gloveText));
+        }
+
+        if (maskRemainingUses <= 0 && maskInUse)
+        {
+            maskInUse = false;
+            StartCoroutine(cannotUse(maskText));
+        }
+    }
+
+    private void updateAllUsesText()
+    {
+        gloveText.text = String.Format("{0} / 3", gloveRemainingUses <= 0 && !itemsAreAvailable ? "--" : gloveRemainingUses);
+
+        alcoholText.text = String.Format("{0} / 2", alcoholRemainingUses <= 0 && !itemsAreAvailable ? "--" : alcoholRemainingUses);
+
+        maskText.text = String.Format("{0} / 3", maskRemainingUses <= 0 && !itemsAreAvailable ? "--" : maskRemainingUses);
+    }
+
+    public void toggleGlove()
+    {
+        if (gloveRemainingUses <= 0)
+        {
+            gloveRemainingUses = 0;
+            StartCoroutine(cannotUse(gloveText));
+            return;
+        }
+
+        glovesInUse = glovesInUse ? false : true;
+    }
+
+    public void toggleMask()
+    {
+        if (maskRemainingUses <= 0)
+        {
+            maskRemainingUses = 0;
+            StartCoroutine(cannotUse(maskText));
+            return;
+        }
+
+        maskInUse = maskInUse ? false : true;
+    }
+
+    public bool useAlcohol()
+    {
+        if (alcoholRemainingUses <= 0)
+        {
+            alcoholRemainingUses = 0;
+            StartCoroutine(cannotUse(alcoholText));
+            return false;
+        }
+
+        // TODO
+        alcoholRemainingUses--;
+
+        return true;
+    }
+
+    private IEnumerator cannotUse(TextMeshProUGUI textOut)
+    {
+        textOut.color = Color.red;
+        yield return new WaitForSeconds(0.4f);
+        textOut.color = Color.white;
+    }
+
+    public void LoadData(GameData data)
+    {
+        gloveRemainingUses = data.gloveUsesData;
+        alcoholRemainingUses = data.alcoholUsesData;
+        maskRemainingUses = data.maskUsesData;
+    }
+
+    public void SaveData(ref GameData data)
+    {
+        data.gloveUsesData = gloveRemainingUses;
+        data.alcoholUsesData = alcoholRemainingUses;
+        data.maskUsesData = maskRemainingUses;
+    }
+}
---
+++ Thesis V2/Assets/Scripts/Managers/Hud Managers/PlayerHealthManager.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    public PlayerHealthManager instance { get; private set; }
+
+    [SerializeField] private Slider healthBar;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one instance of Player Health Manager Exists in the current scene");
+        }
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if (healthBar.value <= 0)
+        {
+            // TODO: Game Over From Infection
+        }
+
+        if (InputManager.getInstance().getAlcoholPressed())
+        {
+            healthBar.value += InventoryManager.instance.useAlcohol() ? 5 : 0;
+        }
+
+        if (InputManager.getInstance().getGlovePressed())
+        {
+            InventoryManager.instance.toggleGlove();
+        }
+
+        if (InputManager.getInstance().getMaskPressed())
+        {
+            InventoryManager.instance.toggleMask();
+        }
+    }
+
+    public void reduceHealthWithGloves()
+    {
+        float reductionValue = 10;
+
+        if (InventoryManager.instance.glovesInUse && InventoryManager.instance.gloveRemainingUses >= 1)
+        {
+            reductionValue = 3;
+        }
+
+        InventoryManager.instance.gloveRemainingUses--;
+        healthBar.value -= reductionValue;
+    }
+
+    public void reduceHealthWithMask()
+    {
+        float reductionValue = 15;
+
+        if (InventoryManager.instance.maskInUse && InventoryManager.instance.maskRemainingUses >= 1)
+        {
+            reductionValue = 5;
+        }
+
+        InventoryManager.instance.maskRemainingUses--;
+        healthBar.value -= reductionValue;
+    }
+
+    public void fullRestore()
+    {
+        healthBar.value = healthBar.value.max;
+        InventoryManager.instance.alcoholRemainingUses = 2;
+        InventoryManager.instance.gloveRemainingUses = 3;
+        InventoryManager.instance.maskRemainingUses = 3;
+    }
+}
