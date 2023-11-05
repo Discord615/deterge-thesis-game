11b6ccc79665685362df53cfe4dd38261aabb05e
Discord615 <vanbeau1@gmail.com>
2023-11-05 18:07:26 +0800

    Added line 34
    This checks if npc is sick or laying down. and if true then dont let the player interact with the npc.

--- Thesis V2/Assets/Scripts/Dialogue/DialogueAction.cs
+++ Thesis V2/Assets/Scripts/Dialogue/DialogueAction.cs
@@ -16,44 +16,56 @@ public class DialogueAction : MonoBehaviour, IDataPersistence
 
     [SerializeField] private string id;
 
-	[ContextMenu("Generate guid for id")]
-	private void GenerateGuid()
-	{
-		id = System.Guid.NewGuid().ToString();
-	}
+    [ContextMenu("Generate guid for id")]
+    private void GenerateGuid()
+    {
+        id = System.Guid.NewGuid().ToString();
+    }
 
     public bool playerInRange;
 
-    private void Awake(){
+    private void Awake()
+    {
         visualCue.SetActive(false);
     }
 
-    void Update(){
-        if (!playerInRange){
+    void Update()
+    {
+        if (gameObject.GetComponent<NPCAnimScript>().isSick || gameObject.GetComponent<NPCAnimScript>().isLayingDown) return;
+
+        if (!playerInRange)
+        {
             visualCue.SetActive(false);
             return;
         }
 
         visualCue.SetActive(true);
 
-        if (!InputManager.getInstance().GetInteractPressed()) {
+        if (!InputManager.getInstance().GetInteractPressed())
+        {
             return;
         }
 
         DialogueManagaer.GetInstance().EnterDialogueMode(inkJson);
     }
 
-    public void LoadData(GameData data){
+    public void LoadData(GameData data)
+    {
         TextAsset output;
-        if (data.inkJsonData.TryGetValue(id, out output)){
+        if (data.inkJsonData.TryGetValue(id, out output))
+        {
             this.inkJson = output;
-        } else {
+        }
+        else
+        {
             this.inkJson = InkManager.instance.getRandomInk(isMale);
         }
     }
 
-    public void SaveData(ref GameData data){
-        if (data.inkJsonData.ContainsKey(id)){
+    public void SaveData(ref GameData data)
+    {
+        if (data.inkJsonData.ContainsKey(id))
+        {
             data.inkJsonData.Remove(id);
         }
         data.inkJsonData.Add(id, inkJson);
