51ac20cee19f682abb966033c2972717dd3c95d9
Discord615 <vanbeau1@gmail.com>
2023-11-22 03:53:08 +0800

    Added some unstuck scripts

--- Thesis V2/Assets/Scripts/NPC Scripts/UnstuckScript.cs
+++ Thesis V2/Assets/Scripts/NPC Scripts/UnstuckScript.cs
@@ -6,9 +6,21 @@ public class UnstuckScript : MonoBehaviour
 {
     private Vector3 currentPosition;
     private float waitingTime = 4f;
+    private float secondWait = 6f;
     private void FixedUpdate() {
-        if (!GetComponent<NPCAnimScript>().isLayingDown){
-            waitingTime -= Time.deltaTime;
+        if (GetComponent<NPCAnimScript>().isLayingDown){
+            secondWait = 6f;
+            waitingTime = 4f;
+            return;
+        }
+
+        waitingTime -= Time.deltaTime;
+        secondWait -= Time.deltaTime;
+
+        if (secondWait <= 0){
+            GetComponent<Unit>().floor = GetComponent<Unit>().floor == 1 ? 2 : 1;
+            GetComponent<NPCAnimScript>().stopped = true;
+            secondWait = 6f;
         }
 
         if (waitingTime <= 0){
@@ -18,6 +30,7 @@ public class UnstuckScript : MonoBehaviour
 
         if (Mathf.CeilToInt(currentPosition.x) == Mathf.CeilToInt(transform.position.x) && Mathf.CeilToInt(currentPosition.z) == Mathf.CeilToInt(transform.position.z)) return;
             currentPosition = transform.position;
+            secondWait = 6f;
             waitingTime = 4f;
     }
 }
