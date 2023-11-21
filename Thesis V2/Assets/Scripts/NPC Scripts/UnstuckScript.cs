3adea1293488ed8f0b6c3f20652089ae91225a6e
Discord615 <vanbeau1@gmail.com>
2023-11-22 03:21:15 +0800

    Update UnstuckScript.cs

--- Thesis V2/Assets/Scripts/NPC Scripts/UnstuckScript.cs
+++ Thesis V2/Assets/Scripts/NPC Scripts/UnstuckScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class UnstuckScript : MonoBehaviour
 {
     private Vector3 currentPosition;
-    private float waitingTime = 6f;
+    private float waitingTime = 4f;
     private void FixedUpdate() {
         if (!GetComponent<NPCAnimScript>().isLayingDown){
             waitingTime -= Time.deltaTime;
@@ -13,11 +13,11 @@ public class UnstuckScript : MonoBehaviour
 
         if (waitingTime <= 0){
             GetComponent<NPCAnimScript>().stopped = true;
-            waitingTime = 6f;
+            waitingTime = 4f;
         }
 
         if (Mathf.CeilToInt(currentPosition.x) == Mathf.CeilToInt(transform.position.x) && Mathf.CeilToInt(currentPosition.z) == Mathf.CeilToInt(transform.position.z)) return;
             currentPosition = transform.position;
-            waitingTime = 6f;
+            waitingTime = 4f;
     }
 }
