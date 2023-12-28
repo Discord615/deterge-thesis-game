218e609f576e7c688c4cf6bc53b5b9db5daa4f78
Discord615 <vanbeau1@gmail.com>
2023-12-28 23:37:49 +0800

    Changed WinScreen to Prompt/TransitionPrompt SCreen
    TODO:
    Add script for the new Prompt Screen

---
+++ Thesis V2/Assets/Scripts/Menus/PromptScript.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PromptScript : MonoBehaviour
+{
+    // ! Add event where if triggered, shows Prompt Screen.
+}
